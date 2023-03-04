bd3eefc51f7fd9ec0b208a6329c8e6c2af79b635
rat211 <123985663+rat211@users.noreply.github.com>
2023-03-05 00:25:33 +0530

    Create Q1.java
    version 1 - ratan

---
+++ Q1.java
@@ -0,0 +1,56 @@
+// Java assignment Q.1
+
+import java.util.Scanner;
+
+class Mobike
+{
+	Scanner s = new Scanner(System.in);
+	int bno, days, charge;
+	long phno;
+	String name;
+	void input()
+	{
+		System.out.print("Name of Customer: ");
+		name = s.nextLine();
+		System.out.print("Phone no.: ");
+		phno = s.nextLong();
+		System.out.print("Bike no.: ");
+		bno = s.nextInt();
+		System.out.print("Number of days bike taken on rent: ");
+		days = s.nextInt();
+	}
+	void compute()
+	{
+		if(days<=5)
+		{
+			charge = 500*days;
+		}
+		else if(days>=6 && days<=10)
+		{
+			charge = 400*days;
+		}
+		else if(days>10)
+		{
+			charge = 200*days;
+		}
+	}
+	void display()
+	{
+		System.out.println("\n==================");
+		System.out.println("Bike no.: "+bno);
+		System.out.println("Phone no.: "+phno);
+		System.out.println("No. of days: "+days);
+		System.out.println("Charge: "+charge);
+	}
+}
+
+class Main
+{
+	public static void main(String z[])
+	{
+		Mobike m = new Mobike();
+		m.input();
+		m.compute();
+		m.display();
+	}
+}
